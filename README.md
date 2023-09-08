6473d7d82eac6a3d34a3b9c602851b876acdc065
Louis Tremblay Thibault <tremblaythibaultl@gmail.com>
2023-09-08 16:38:01 +0200

    cleanup

---
+++ README.md
@@ -0,0 +1,5 @@
+# About
+
+This library hosts a toy implementation of the [[Joye22]](https://tches.iacr.org/index.php/TCHES/article/view/9836) paper. The variable names aim to follow those in the paper.
+
+The library is not safe.
--- src/lib.rs
+++ src/lib.rs
@@ -5,5 +5,4 @@ pub mod poly;
 pub const ELL: usize = 2; // number of decomposition layers
 #[allow(non_upper_case_globals)]
 pub const k: usize = 1; // GLWE dimension
-                        // The external product currently only works with k = 1
 pub const N: usize = 1024; // degree N of irreducible polynomial X^N + 1
